06829fbd364936c30e157158bb23def88862f950
Nicole Mah <mnmah@alaska.edu>
2023-01-22 17:01:15 -0900

    Blog Details

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react";
 
 const useFetch = (url) => {
-    const [data, setData] = useState(null);
+	const [data, setData] = useState(null);
 	const [isPending, setIsPending] = useState(true);
 	const [error, setError] = useState(null);
 
 	useEffect(() => {
-		console.log("use effect ran");
-		fetch(url)
+		const abortCont = new AbortController();
+
+		fetch(url, { signal: abortCont.signal })
 			.then((res) => {
 				if (!res.ok) {
 					throw Error("Could not fetch the data for that resource");
@@ -20,11 +21,17 @@ const useFetch = (url) => {
 				setError(null);
 			})
 			.catch((err) => {
-				setIsPending(false);
-				setError(err.message);
+				if (err.name === "AbortError") {
+					console.log("fetch aborted");
+				} else {
+					setIsPending(false);
+					setError(err.message);
+				}
 			});
+
+		return () => abortCont.abort();
 	}, [url]);
-    return { data, isPending, error}
-}
+	return { data, isPending, error };
+};
 
 export default useFetch;
